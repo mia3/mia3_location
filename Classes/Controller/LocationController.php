815f93ab1faf7d1f0599181d72982a4c618333d7
Marc Neuhaus <mneuhaus@famelo.com>
2016-03-31 14:08:46 +0200

    prevent empty rows from being shown

--- Classes/Controller/LocationController.php
+++ Classes/Controller/LocationController.php
@@ -147,6 +147,9 @@ class LocationController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControl
             $additionalLocations = $this->locationRepository->findNearBy($address, $latitude, $longitude, 999);
             while (count($locations) < $this->settings['showAtLeast']) {
             	$location =  array_shift($additionalLocations);
+            	if (!$location instanceof Location) {
+            		break;
+            	}
                 $locations[$location->getUid()] = $location;
             }
         }
